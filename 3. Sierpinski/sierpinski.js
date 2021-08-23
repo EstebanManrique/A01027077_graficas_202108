1f96847537f32d4d40020bfa8c3ae65a7e9fad3e
EstebanManrique <a01027077@itesm.mx>
2021-08-22 21:59:35 -0500

    Sierpinski terminado y comentado

--- 3. Sierpinski/sierpinski.js
+++ 3. Sierpinski/sierpinski.js
@@ -1,11 +1,27 @@
-import {triangle} from "./triangle.js";
+import {
+    triangle
+} from "./triangle.js"; //Se importa clase donde se dibuja triangulos y se tiene metodo recursivo para dibujarlos
 
-function main(){
-    const canvas = document.getElementById("sierpinskiCanvas")
-    const contexto = canvas.getContext("2d");
+function update(triangulo, contexto, slider, texto) { //Funcion de update de texto debajo de slider, al igual que de triangulo segun slider
+    requestAnimationFrame(() => update(triangulo, contexto, slider, texto));
 
-    let triangulo = new triangle(1000,500,-500);
-    triangulo.drawSierpinski(contexto,triangulo.x, triangulo.y, triangulo.sideLength, 8)
+    contexto.clearRect(0, 0, 2000, 1000)
+    let textoBase = "Numero de divisiones: "
+    texto.innerText = textoBase.concat((slider.value).toString()) //Actualizacion de texto de acuerdo a slider
+
+    triangulo.drawSierpinski(contexto, triangulo.x, triangulo.y, triangulo.sideLength, slider.value) //Actualizacion de triangulo segun numero en slider
+}
+
+function main() {
+    const canvas = document.getElementById("sierpinskiCanvas") //Se obtiene canvas
+    const contexto = canvas.getContext("2d"); //Generación de contexto
+    const slider = document.getElementById("sierpinskiSlider") //Se obtiene elemento de slider
+    const texto = document.getElementById("textoSlider") //Se obtiene elemento de texto debajo de slider
+
+    let triangulo = new triangle(1000, 550, -560); //Dibuja el Triangulo dentro de los cuales se dibujaran el resto segun el slider
+    triangulo.drawSierpinski(contexto, triangulo.x, triangulo.y, triangulo.sideLength, slider.value) //Funcion recursiva para dibujar los diferentes niveles de triangulos
+
+    update(triangulo, contexto, slider, texto) //Se llama funcion de update del canvas
 }
 
 main()
--- 3. Sierpinski/triangle.js
+++ 3. Sierpinski/triangle.js
@@ -1,35 +1,39 @@
-class triangle{
-    constructor(x,y,sideLength){
-        this.x = x;
-        this.y = y;
-        this.sideLength = sideLength;
+//Clase para triangulos y recursión para Sierpinski
+
+//Se uso el siguiente link como apoyo para elaborar este script: https://xtrp.io/blog/2020/11/20/generating-the-sierpinski-triangle-in-vanilla-javascript-with-html5-canvas/
+
+class triangle {
+    constructor(x, y, sideLength) { //Constructor del triangulo dentro de los cuales se dibujaran el resto
+        this.x = x; //Coordenada en x del vertice inferior izquierdo del triangulo mas grande
+        this.y = y; //Coordenada en y del vertice inferior izquierdo del triangulo mas grande
+        this.sideLength = sideLength; //Longitud del triangulo mas grande 
     }
 
-    draw(contexto,x,y,sideLength,color){
+    draw(contexto, x, y, sideLength, color) { //Metodo con el cual se dibujan los triangulos
         contexto.beginPath();
-        contexto.moveTo(x, y);
-        contexto.lineTo(x + (sideLength/2), y + sideLength * Math.sin(Math.PI/3))
-        contexto.lineTo(x + sideLength, y);
-        contexto.lineTo(x, y);
+        contexto.moveTo(x, y); //Se establece comienzo de dibujo en el punto x,y
+        contexto.lineTo(x + (sideLength / 2), y + sideLength * Math.sin(Math.PI / 3)) //Se dibuja primera arista
+        contexto.lineTo(x + sideLength, y); //Se dibuja segunda arista
+        contexto.lineTo(x, y); //Se dibuja tercera arista
         contexto.closePath();
-        contexto.fillStyle = color
+        contexto.fillStyle = color //Se rellena triangulo con este color
         contexto.fill()
     }
 
-    drawSierpinski(contexto,x,y,sideLength,divisiones){
-        let sideLengthMitad = (sideLength / 2);
-        let posiconesVertices = [x + (sideLengthMitad/2), y + sideLengthMitad * Math.sin(Math.PI/3),
-                                 x + sideLengthMitad, y, x, y]
-        if(divisiones != 0){
-            for(let i = 0; i<3; i++){
-                this.drawSierpinski(contexto,posiconesVertices[(2*i)],posiconesVertices[(2*i)+1],sideLengthMitad,divisiones-1)
+    drawSierpinski(contexto, x, y, sideLength, divisiones) { //Funcion recursiva con la cual se pueden hacer los diferentes niveles triangulos
+        let sideLengthMitad = (sideLength / 2); // Se obtiene la mitad de la longitud dada por cada nivel de triangulos que se vaya a hacer
+        let posiconesVertices = [x + (sideLengthMitad / 2), y + sideLengthMitad * Math.sin(Math.PI / 3),
+            x + sideLengthMitad, y, x, y
+        ] //Este arreglo sirve para poder definir tres diferentes vertices de los cuales se comienzan a dibujar diferentes triangulos en cada uno de los niveles
+        if (divisiones != 0) { //En caso de que aun haya niveles por dibujar, se llama funcion recursiva
+            for (let i = 0; i < 3; i++) {
+                this.drawSierpinski(contexto, posiconesVertices[(2 * i)], posiconesVertices[(2 * i) + 1], sideLengthMitad, divisiones - 1) //Funcion recursiva
             }
-        }
-        else{
-            for(let i = 0; i<3; i++){
-                this.draw(contexto,posiconesVertices[(2*i)],posiconesVertices[(2*i)+1],sideLengthMitad,"tomato")
-            }
+        } else { //En caso de que hay un numero de divisiones 0, se dibuja triangulo sencillo
+            this.draw(contexto, x, y, sideLength, "firebrick") //Dibjuo de triangulo sencillo
         }
     }
 }
-export {triangle};
+export {
+    triangle
+};
